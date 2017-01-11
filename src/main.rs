cdf0a9911d1d225e19c9e04b86a53991b7ff62fa
Jordan Lewis <jordanthelewis@gmail.com>
2017-01-10 18:45:15 -0700

    run through rustfmt

--- src/main.rs
+++ src/main.rs
@@ -43,22 +43,22 @@ pub enum Instr {
 }
 
 struct Program {
-  instrs: Vec<Instr>
+  instrs: Vec<Instr>,
 }
 
 struct Spec {
-  programs: Vec<Program>
+  programs: Vec<Program>,
 }
 
 pub fn parse_loc(s: &str) -> Result<Loc, String> {
   match s {
-    "LEFT" => { Ok(Loc::Left) }
-    "RIGHT" => { Ok(Loc::Right) }
-    "UP" => { Ok(Loc::Up) }
-    "DOWN" => { Ok(Loc::Down) }
-    "ACC" => { Ok(Loc::Acc) }
-    "LAST" => { Ok(Loc::Last) }
-    s => { Err(format!("Invalid loc {}", s)) }
+    "LEFT" => Ok(Loc::Left),
+    "RIGHT" => Ok(Loc::Right),
+    "UP" => Ok(Loc::Up),
+    "DOWN" => Ok(Loc::Down),
+    "ACC" => Ok(Loc::Acc),
+    "LAST" => Ok(Loc::Last),
+    s => Err(format!("Invalid loc {}", s)),
   }
 }
 
@@ -68,8 +68,8 @@ pub fn parse_lit(s: &str) -> Result<i16, String> {
 
 pub fn parse_source(s: &str) -> Result<Source, String> {
   parse_loc(s)
-      .map(Source::Loc)
-      .or_else(|_| parse_lit(s).map(Source::Val))
+    .map(Source::Loc)
+    .or_else(|_| parse_lit(s).map(Source::Val))
 }
 
 fn eat_comma(s: &str) -> Result<&str, String> {
@@ -81,72 +81,87 @@ fn eat_comma(s: &str) -> Result<&str, String> {
 }
 
 pub fn parse_mov(words: &mut SplitWhitespace) -> Result<Instr, String> {
-  let src = try!(
-    words.next()
-         .ok_or("invalid mov instruction without source".to_string())
-         .and_then(eat_comma)
-         .and_then(parse_source));
-  let dest = try!(
-    words.next()
-         .ok_or("invalid mov instruction without dest".to_string())
-         .and_then(parse_loc));
+  let src = try!(words.next()
+    .ok_or("invalid mov instruction without source".to_string())
+    .and_then(eat_comma)
+    .and_then(parse_source));
+  let dest = try!(words.next()
+    .ok_or("invalid mov instruction without dest".to_string())
+    .and_then(parse_loc));
   Ok(Instr::Mov(src, dest))
 }
 
 fn parse_line(line: String) -> Result<Instr, String> {
   let mut words = line.split_whitespace();
   match words.next() {
-    Some("NOP") => { Ok(Instr::Nop) }
-    Some("MOV") => { parse_mov(&mut words) }
-    Some("SWP") => { Ok(Instr::Swp) }
-    Some("SAV") => { Ok(Instr::Sav) }
-    Some("ADD") => { words.next()
-                          .ok_or("invalid add instr without source".to_string())
-                          .and_then(parse_source)
-                          .map(Instr::Add) }
-    Some("SUB") => { words.next()
-                          .ok_or("invalid sub instr without source".to_string())
-                          .and_then(parse_source)
-                          .map(Instr::Sub) }
-    Some("NEG") => { Ok(Instr::Neg) }
-    Some("JMP") => { words.next()
-                          .ok_or("invalid jmp instr without label".to_string())
-                          .map(str::to_string)
-                          .map(Instr::Jmp) }
-    Some("JEZ") => { words.next()
-                          .ok_or("invalid jez instr without label".to_string())
-                          .map(str::to_string)
-                          .map(Instr::Jez) }
-    Some("JNZ") => { words.next()
-                          .ok_or("invalid jnz instr without label".to_string())
-                          .map(str::to_string)
-                          .map(Instr::Jnz) }
-    Some("JGZ") => { words.next()
-                          .ok_or("invalid jgz instr without label".to_string())
-                          .map(str::to_string)
-                          .map(Instr::Jgz) }
-    Some("JLZ") => { words.next()
-                          .ok_or("invalid jlz instr without label".to_string())
-                          .map(str::to_string)
-                          .map(Instr::Jlz) }
-    Some("JRO") => { words.next()
-                          .ok_or("invalid jro instr without source".to_string())
-                          .and_then(parse_source)
-                          .map(Instr::Jro) }
+    Some("NOP") => Ok(Instr::Nop),
+    Some("MOV") => parse_mov(&mut words),
+    Some("SWP") => Ok(Instr::Swp),
+    Some("SAV") => Ok(Instr::Sav),
+    Some("ADD") => {
+      words.next()
+        .ok_or("invalid add instr without source".to_string())
+        .and_then(parse_source)
+        .map(Instr::Add)
+    }
+    Some("SUB") => {
+      words.next()
+        .ok_or("invalid sub instr without source".to_string())
+        .and_then(parse_source)
+        .map(Instr::Sub)
+    }
+    Some("NEG") => Ok(Instr::Neg),
+    Some("JMP") => {
+      words.next()
+        .ok_or("invalid jmp instr without label".to_string())
+        .map(str::to_string)
+        .map(Instr::Jmp)
+    }
+    Some("JEZ") => {
+      words.next()
+        .ok_or("invalid jez instr without label".to_string())
+        .map(str::to_string)
+        .map(Instr::Jez)
+    }
+    Some("JNZ") => {
+      words.next()
+        .ok_or("invalid jnz instr without label".to_string())
+        .map(str::to_string)
+        .map(Instr::Jnz)
+    }
+    Some("JGZ") => {
+      words.next()
+        .ok_or("invalid jgz instr without label".to_string())
+        .map(str::to_string)
+        .map(Instr::Jgz)
+    }
+    Some("JLZ") => {
+      words.next()
+        .ok_or("invalid jlz instr without label".to_string())
+        .map(str::to_string)
+        .map(Instr::Jlz)
+    }
+    Some("JRO") => {
+      words.next()
+        .ok_or("invalid jro instr without source".to_string())
+        .and_then(parse_source)
+        .map(Instr::Jro)
+    }
     Some(s) => {
       if s.starts_with("#") {
         Ok(Instr::Comment(words.fold(s.to_string(), |acc, s| acc + " " + s)))
       } else if s.starts_with("@") {
-        s[1..].parse::<u8>()
-            .map_err(|e| e.to_string())
-            .map(Instr::Section)
+        s[1..]
+          .parse::<u8>()
+          .map_err(|e| e.to_string())
+          .map(Instr::Section)
       } else if s.ends_with(":") {
         Ok(Instr::Label(s[..s.len() - 1].to_string()))
       } else {
         Err(format!("invalid instr {}", s))
       }
     }
-    None => { Ok(Instr::Comment("".to_string())) }
+    None => Ok(Instr::Comment("".to_string())),
   }
 }
 
@@ -160,18 +175,18 @@ fn parse_spec(buf: &mut BufRead) -> Result<Spec, String> {
     match instr {
       Instr::Section(i) => {
         if i != next_section {
-          return Err(format!("expecting section {}, found section {}", next_section, i))
+          return Err(format!("expecting section {}, found section {}", next_section, i));
         } else if i > 11 {
-          return Err(format!("section {} greater than maximum 11", i))
+          return Err(format!("section {} greater than maximum 11", i));
         }
         next_section = next_section + 1;
-        programs.push(Program{instrs: instrs});
+        programs.push(Program { instrs: instrs });
         instrs = Vec::new();
       }
-      i => instrs.push(i)
+      i => instrs.push(i),
     }
   }
-  Ok(Spec{programs: programs})
+  Ok(Spec { programs: programs })
 }
 
 fn main() {
@@ -187,25 +202,26 @@ mod tests {
 
   #[test]
   fn test_parse_lit() {
-    assert_eq!(Ok(3),  parse_lit("3"));
+    assert_eq!(Ok(3), parse_lit("3"));
     assert!(parse_lit("f").is_err());
   }
 
   #[test]
   fn test_parse_loc() {
-    assert_eq!(Ok(Loc::Acc),  parse_loc("ACC"));
+    assert_eq!(Ok(Loc::Acc), parse_loc("ACC"));
     assert!(parse_loc("Aflkj").is_err());
   }
 
   #[test]
   fn test_parse_source() {
-    assert_eq!(Ok(Source::Loc(Loc::Acc)),  parse_source("ACC"));
+    assert_eq!(Ok(Source::Loc(Loc::Acc)), parse_source("ACC"));
     assert_eq!(Ok(Source::Val(3)), parse_source("3"));
     assert!(parse_source("Aflkj").is_err());
   }
 
   #[test]
   fn test_parse_mov() {
-    assert_eq!(Ok(Instr::Mov(Source::Val(3), Loc::Acc)),  parse_mov(&mut "3, ACC".split_whitespace()));
+    assert_eq!(Ok(Instr::Mov(Source::Val(3), Loc::Acc)),
+               parse_mov(&mut "3, ACC".split_whitespace()));
   }
 }
